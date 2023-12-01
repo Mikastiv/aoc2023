f84c8c77bd4c4124a6b7d16663745079d8b18f73
mikastiv <mikastiv@outlook.com>
2023-12-01 12:11:36 -0500

    day1 done

--- src/day1.zig
+++ src/day1.zig
@@ -1,7 +1,58 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const input = @embedFile("day1");
 
-pub fn main() void {
-    std.debug.print("{s}", .{input});
+const lettered_numbers = [_][]const u8{
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+};
+
+fn getCalibration(values: *const std.ArrayList(u8)) u32 {
+    return values.items[0] * 10 + values.getLast();
+}
+
+fn getLetteredNumber(str: []const u8) ?u8 {
+    for (lettered_numbers, 0..) |number, value| {
+        if (std.mem.startsWith(u8, str, number)) return @intCast(value + 1);
+    }
+
+    return null;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+
+    var digits1 = std.ArrayList(u8).init(allocator);
+    var digits2 = std.ArrayList(u8).init(allocator);
+
+    var calibration1: u32 = 0;
+    var calibration2: u32 = 0;
+
+    while (lines.next()) |line_raw| {
+        const line = if (builtin.os.tag == .windows) std.mem.trim(u8, line_raw, "\r") else line_raw;
+
+        for (0..line.len) |i| {
+            switch (line[i]) {
+                '0'...'9' => {
+                    try digits1.append(line[i] - '0');
+                    try digits2.append(line[i] - '0');
+                },
+                else => if (getLetteredNumber(line[i..])) |num| {
+                    try digits2.append(num);
+                },
+            }
+        }
+
+        calibration1 += getCalibration(&digits1);
+        calibration2 += getCalibration(&digits2);
+
+        digits1.clearRetainingCapacity();
+        digits2.clearRetainingCapacity();
+    }
+
+    std.debug.print("part1: {d}\n", .{calibration1});
+    std.debug.print("part2: {d}\n", .{calibration2});
 }
