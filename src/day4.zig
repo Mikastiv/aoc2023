c237f36e200a42bdbe09fdfaa08f8d34a99ebe7c
mikastiv <mikastiv@outlook.com>
2023-12-04 12:38:51 -0500

    remove card struct

--- src/day4.zig
+++ src/day4.zig
@@ -14,30 +14,24 @@ fn createNumberSet(alloc: std.mem.Allocator, str: []const u8) !std.AutoHashMap(u
     return out;
 }
 
-const Card = struct {
-    points: usize,
+fn getPoints(alloc: std.mem.Allocator, str: []const u8) !usize {
+    var split = std.mem.tokenizeScalar(u8, str, ':');
+    _ = split.next();
 
-    fn fromStr(alloc: std.mem.Allocator, str: []const u8) !Card {
-        var split = std.mem.tokenizeScalar(u8, str, ':');
-        _ = split.next();
+    const cards_str = split.next().?;
+    var cards = std.mem.tokenizeScalar(u8, cards_str, '|');
 
-        const cards_str = split.next().?;
-        var cards = std.mem.tokenizeScalar(u8, cards_str, '|');
+    const winning = try createNumberSet(alloc, cards.next().?);
+    const numbers = try createNumberSet(alloc, cards.next().?);
 
-        const winning = try createNumberSet(alloc, cards.next().?);
-        const numbers = try createNumberSet(alloc, cards.next().?);
-
-        var points: usize = 0;
-        var it = winning.keyIterator();
-        while (it.next()) |key| {
-            if (numbers.contains(key.*)) points += 1;
-        }
-
-        return .{
-            .points = points,
-        };
+    var points: usize = 0;
+    var it = winning.keyIterator();
+    while (it.next()) |key| {
+        if (numbers.contains(key.*)) points += 1;
     }
-};
+
+    return points;
+}
 
 pub fn main() !void {
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
@@ -47,19 +41,19 @@ pub fn main() !void {
 
     var cards = [_]u32{1} ** 256;
 
-    var points: usize = 0;
+    var total_points: usize = 0;
     var total_cards: usize = 0;
     var id: u32 = 0;
     while (lines.next()) |line_raw| : (id += 1) {
         const line = if (builtin.os.tag == .windows) std.mem.trim(u8, line_raw, "\r") else line_raw;
 
-        const card = try Card.fromStr(allocator, line);
-        points += if (card.points == 0) 0 else std.math.pow(usize, 2, card.points - 1);
+        const points = try getPoints(allocator, line);
+        total_points += if (points == 0) 0 else std.math.pow(usize, 2, points - 1);
 
         const count = cards[id];
         total_cards += count;
 
-        const extra_cards = card.points;
+        const extra_cards = points;
         for (0..count) |_| {
             for (0..extra_cards) |i| {
                 cards[id + i + 1] += 1;
@@ -67,6 +61,6 @@ pub fn main() !void {
         }
     }
 
-    std.debug.print("part 1: {d}\n", .{points});
+    std.debug.print("part 1: {d}\n", .{total_points});
     std.debug.print("part 2: {d}\n", .{total_cards});
 }
