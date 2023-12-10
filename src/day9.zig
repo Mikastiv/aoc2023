369653b26c6629492f8baeba4b38436ca9d42981
mikastiv <mikastiv@outlook.com>
2023-12-09 22:47:32 -0500

    refactor

--- src/day9.zig
+++ src/day9.zig
@@ -3,8 +3,6 @@ const utils = @import("utils.zig");
 
 const input = @embedFile("input");
 
-var buffer: [4096 * 2]u8 = undefined;
-
 fn solve(alloc: std.mem.Allocator, numbers: []i64) !struct { i64, i64 } {
     var arrays = std.ArrayList(std.ArrayList(i64)).init(alloc);
     try arrays.append(std.ArrayList(i64).fromOwnedSlice(alloc, numbers));
@@ -12,39 +10,40 @@ fn solve(alloc: std.mem.Allocator, numbers: []i64) !struct { i64, i64 } {
     var i: u64 = 0;
     while (std.mem.indexOfNone(i64, arrays.items[i].items, &[_]i64{0}) != null) : (i += 1) {
         const it = arrays.items[i];
-        var next_row = try std.ArrayList(i64).initCapacity(alloc, it.items.len);
-        for (1..it.items.len) |j| {
-            try next_row.append(it.items[j] - it.items[j - 1]);
+        var next_row = try std.ArrayList(i64).initCapacity(alloc, it.items.len + 2);
+        var windows = std.mem.window(i64, it.items, 2, 1);
+        while (windows.next()) |window| {
+            try next_row.append(window[1] - window[0]);
         }
         try arrays.append(next_row);
     }
 
+    var last_numbers = try std.ArrayList(i64).initCapacity(alloc, arrays.items.len);
+    var first_numbers = try std.ArrayList(i64).initCapacity(alloc, arrays.items.len);
+    for (arrays.items) |row| {
+        try first_numbers.append(row.items[0]);
+        try last_numbers.append(row.getLast());
+    }
+
     // part 1
-    i = arrays.items.len - 1;
-    try arrays.items[i].append(0);
-    while (i > 0) : (i -= 1) {
-        const it = arrays.items[i];
-        const j = it.items.len - 1;
-        const curr = it.items[j];
-        const prev = arrays.items[i - 1].items[j];
-        try arrays.items[i - 1].append(prev + curr);
+    std.mem.reverse(i64, last_numbers.items);
+    var part1: i64 = 0;
+    for (last_numbers.items) |item| {
+        part1 += item;
     }
 
     // part 2
-    i = arrays.items.len - 1;
-    try arrays.items[i].insert(0, 0);
-    while (i > 0) : (i -= 1) {
-        const it = arrays.items[i];
-        const j = 0;
-        const curr = it.items[j];
-        const prev = arrays.items[i - 1].items[j];
-        try arrays.items[i - 1].insert(0, prev - curr);
+    std.mem.reverse(i64, first_numbers.items);
+    var part2: i64 = 0;
+    for (first_numbers.items) |item| {
+        part2 = item - part2;
     }
 
-    return .{ arrays.items[0].getLast(), arrays.items[0].items[0] };
+    return .{ part1, part2 };
 }
 
 pub fn main() !void {
+    var buffer: [8192]u8 = undefined;
     var fixed = std.heap.FixedBufferAllocator.init(&buffer);
     const alloc = fixed.allocator();
 
