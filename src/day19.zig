fe10b8a40c17a3e2691560318d70b9c072b68b17
mikastiv <mikastiv@outlook.com>
2023-12-21 01:19:33 -0500

    day 19

--- src/day19.zig
+++ src/day19.zig
@@ -69,6 +69,11 @@ const Rating = struct {
     value: u16,
 };
 
+const Range = struct {
+    lo: u64,
+    hi: u64,
+};
+
 fn parseWorkflows(alloc: std.mem.Allocator, str: []const u8) !std.StringHashMap([]Entry) {
     var lines = std.mem.tokenizeScalar(u8, str, '\n');
     var workflows = std.StringHashMap([]Entry).init(alloc);
@@ -140,6 +145,57 @@ fn partAccepted(ratings: [4]Rating, workflows: *const std.StringHashMap([]Entry)
     return false;
 }
 
+fn countParts(workflows: *const std.StringHashMap([]Entry), ranges: [4]Range, name: []const u8) u64 {
+    if (std.mem.eql(u8, name, "R")) return 0;
+    if (std.mem.eql(u8, name, "A")) {
+        var result: u64 = 1;
+        for (ranges) |range| {
+            result *= range.hi - range.lo + 1;
+        }
+        return result;
+    }
+
+    var ranges_mut = ranges;
+    var total: u64 = 0;
+    const workflow = workflows.get(name).?;
+    for (workflow) |entry| {
+        switch (entry) {
+            .default => |d| {
+                total += countParts(workflows, ranges_mut, d);
+            },
+            .rule => |rule| {
+                const idx: usize = @intFromEnum(rule.part);
+                const range = ranges_mut[idx];
+
+                var to_next_workflow: Range = undefined;
+                var to_next_rule: Range = undefined;
+                if (rule.cmp == .lt) {
+                    to_next_workflow = .{ .lo = range.lo, .hi = @min(rule.value - 1, range.hi) };
+                    to_next_rule = .{ .lo = @max(rule.value, range.lo), .hi = range.hi };
+                } else {
+                    to_next_workflow = .{ .lo = @max(rule.value + 1, range.lo), .hi = range.hi };
+                    to_next_rule = .{ .lo = range.lo, .hi = @min(rule.value, range.hi) };
+                }
+
+                // range to next workflow
+                if (to_next_workflow.lo < to_next_workflow.hi) {
+                    var copy = ranges_mut;
+                    copy[idx] = to_next_workflow;
+                    total += countParts(workflows, copy, rule.next);
+                }
+
+                // range to next rule
+                if (to_next_rule.lo < to_next_rule.hi)
+                    ranges_mut[idx] = to_next_rule // remove range sent to next workflow
+                else
+                    break; // all parts already sent
+            },
+        }
+    }
+
+    return total;
+}
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const alloc = arena.allocator();
@@ -147,10 +203,16 @@ pub fn main() !void {
     var parts = std.mem.tokenizeSequence(u8, input, "\n\n");
     const workflows = try parseWorkflows(alloc, parts.next().?);
     const ratings = try parseRatings(alloc, parts.next().?);
+
     var part1: usize = 0;
     for (ratings.items) |rating| {
-        if (partAccepted(rating, &workflows)) part1 += sumParts(rating);
+        if (partAccepted(rating, &workflows))
+            part1 += sumParts(rating);
     }
 
+    const ranges = [_]Range{.{ .lo = 1, .hi = 4000 }} ** 4;
+    const part2 = countParts(&workflows, ranges, "in");
+
     std.debug.print("part 1: {d}\n", .{part1});
+    std.debug.print("part 2: {d}\n", .{part2});
 }
